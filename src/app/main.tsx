bfcdfb05a2753b76d00b7c7b19ff5a72f9dd89e6
Niakrist <ak_88dron@inbox.ru>
2025-08-26 16:12:36 +0300

    add questionApi from master

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,21 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-<<<<<<< HEAD
-=======
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/routes";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
->>>>>>> d5535a1ba98a94d028554938546e9b28f2e881cd
 import "./styles/index.css";
-import { Button, Htag, Label, Text } from "@/shared/ui";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { store } from "./store/store";
+import { router } from "./routes/routes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
